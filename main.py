ca44e23e1e80fe92e24c8da73ffc90fe59a8f763
Matt Watkins <matthew.watkins@network.rca.ac.uk>
2021-05-26 12:29:43 +0100

    Added calibration for stationary state (not working)

--- main.py
+++ main.py
@@ -8,23 +8,51 @@ robobit.led_rainbow()
 distance = 0
 accelZ = 0
 accelY = 0
-strength2D = 0
+calibSample = [0] 
+calibSampleSize = 20
+calibDone = False
+
+def calibrateAccel(number:number):                   
+    sampleMax = 0
+    sampleMin = 0
+    if len(calibSample) < calibSampleSize:
+        calibSample.append(number)  
+    #sampleMax = max(calibSample)
+    for value in calibSample:
+        if value > sampleMax:
+            sampleMax = value
+        if value < sampleMin:
+            sampleMin = value
+    serial.write_value("Max : ", sampleMax)
+    serial.write_value("Min : ", sampleMin)
+    serial.write_value("Length : ", len(calibSample))          
+
 
 def on_forever():
+
+    for i in range(calibSampleSize):
+        calibrateAccel(input.acceleration(Dimension.Z))
+        basic.pause(100)
     #robobit.startScanner(0xffff00, 100);
-    robobit.go(RBDirection.FORWARD, 50)
-    distance = robobit.sonar(RBPingUnit.Centimeters);
-    #basic.show_string(str(distance))
-    if (distance <= 6):
-        robobit.stop(RBStopMode.BRAKE)
-        robobit.goms(RBDirection.REVERSE, 50, 400)
-        basic.pause(1000) 
-        robobit.rotatems(RBRobotDirection.LEFT, 60, 400)
+    #robobit.go(RBDirection.FORWARD, 50)
+    #distance = robobit.sonar(RBPingUnit.Centimeters);    
+    #basic.show_number(distance)
+    #if (distance <= 6):
+        #robobit.stop(RBStopMode.BRAKE)
+        ##robobit.goms(RBDirection.REVERSE, 50, 400)
+        #basic.pause(1000) 
+        #robobit.rotatems(RBRobotDirection.LEFT, 60, 400)
     #basic.show_number(input.acceleration(Dimension.Y)) 
-    accelZ = input.acceleration(Dimension.Z) 
-    accelY = input.acceleration(Dimension.Z) 
-    strength = Math.sqrt((accelZ * accelZ) + (accelY * accelY))        
-    serial.write_line("Strength: "+str(strength))
-    serial.write_line("accel Y: "+str(accelY))
-    serial.write_line("accel Z: "+str(accelZ))
+    #accelX = input.acceleration(Dimension.X) 
+    #strength = input.acceleration(Dimension.STRENGTH)        
+    #serial.write_value("strength: ", strength)
+    #serial.write_value("accel X: ", accelX)
+    #serial.write_value("strength3D: ", input.acceleration(Dimension.STRENGTH))
+    #serial.write_value("distance: ", distance)
+    #led.plot_bar_graph(accelZ, 1023)
+    #serial.write_value("accel Z: ", accelZ)
+    #if input.acceleration(Dimension.STRENGTH) >= 1023 and input.acceleration(Dimension.STRENGTH) <= 1049 and distance <= 4 :
+        #serial.write_line("STATIONARY")
+    #else:
+        #serial.write_line("MOVING")
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -7,24 +7,57 @@ robobit.ledRainbow()
 let distance = 0
 let accelZ = 0
 let accelY = 0
-let strength2D = 0
-basic.forever(function on_forever() {
-    // robobit.startScanner(0xffff00, 100);
-    robobit.go(RBDirection.Forward, 50)
-    let distance = robobit.sonar(RBPingUnit.Centimeters)
-    // basic.show_string(str(distance))
-    if (distance <= 6) {
-        robobit.stop(RBStopMode.Brake)
-        robobit.goms(RBDirection.Reverse, 50, 400)
-        basic.pause(1000)
-        robobit.rotatems(RBRobotDirection.Left, 60, 400)
+let calibSample = [0]
+let calibSampleSize = 20
+let calibDone = false
+function calibrateAccel(number: number) {
+    let sampleMax = 0
+    let sampleMin = 0
+    if (calibSample.length < calibSampleSize) {
+        calibSample.push(number)
     }
     
-    // basic.show_number(input.acceleration(Dimension.Y)) 
-    let accelZ = input.acceleration(Dimension.Z)
-    let accelY = input.acceleration(Dimension.Z)
-    let strength = Math.sqrt(accelZ * accelZ + accelY * accelY)
-    serial.writeLine("Strength: " + ("" + strength))
-    serial.writeLine("accel Y: " + ("" + accelY))
-    serial.writeLine("accel Z: " + ("" + accelZ))
+    // sampleMax = max(calibSample)
+    for (let value of calibSample) {
+        if (value > sampleMax) {
+            sampleMax = value
+        }
+        
+        if (value < sampleMin) {
+            sampleMin = value
+        }
+        
+    }
+    serial.writeValue("Max : ", sampleMax)
+    serial.writeValue("Min : ", sampleMin)
+    serial.writeValue("Length : ", calibSample.length)
+}
+
+// robobit.startScanner(0xffff00, 100);
+// robobit.go(RBDirection.FORWARD, 50)
+// distance = robobit.sonar(RBPingUnit.Centimeters);    
+// basic.show_number(distance)
+// if (distance <= 6):
+// robobit.stop(RBStopMode.BRAKE)
+// #robobit.goms(RBDirection.REVERSE, 50, 400)
+// basic.pause(1000) 
+// robobit.rotatems(RBRobotDirection.LEFT, 60, 400)
+// basic.show_number(input.acceleration(Dimension.Y)) 
+// accelX = input.acceleration(Dimension.X) 
+// strength = input.acceleration(Dimension.STRENGTH)        
+// serial.write_value("strength: ", strength)
+// serial.write_value("accel X: ", accelX)
+// serial.write_value("strength3D: ", input.acceleration(Dimension.STRENGTH))
+// serial.write_value("distance: ", distance)
+// led.plot_bar_graph(accelZ, 1023)
+// serial.write_value("accel Z: ", accelZ)
+// if input.acceleration(Dimension.STRENGTH) >= 1023 and input.acceleration(Dimension.STRENGTH) <= 1049 and distance <= 4 :
+// serial.write_line("STATIONARY")
+// else:
+// serial.write_line("MOVING")
+basic.forever(function on_forever() {
+    for (let i = 0; i < calibSampleSize; i++) {
+        calibrateAccel(input.acceleration(Dimension.Z))
+        basic.pause(100)
+    }
 })
