85a1ca454f70266fc2123827da5814aca3acfcba
Matt Watkins <matthew.watkins@network.rca.ac.uk>
2021-05-28 18:14:44 +0100

    Avoid and stationary working to a fashion

--- main.py
+++ main.py
@@ -6,7 +6,9 @@ distance = 0
 accelZ = 0
 accelY = 0
 calibValues = [0,0]
+calibAvg = 0
 calibDone = False
+average = 0
 
 def calibrate():   
     # This calibrates the accelermometer to detect if the robot has come to a stop               
@@ -15,16 +17,19 @@ def calibrate():
     calibSample = [] 
     calibSampleSize = 100
     basic.show_string("C")
-    #robobit.go(RBDirection.FORWARD, 50) 
+    robobit.go(RBDirection.FORWARD, 50) 
     # Push a series of samples of the accelerometer into an array, to detect a broad range of changing movements
     for i in range(calibSampleSize):
         calibSample.append(input.acceleration(Dimension.Z)) 
         #serial.write_value("values: ", calibSample[i])
         basic.pause(50)
-
-    
-    #obobit.stop(RBStopMode.BRAKE)
-    #robobit.goms(RBDirection.REVERSE, 50, 1000)
+    # Get average of sample values
+    figure = 0
+    for i in range (calibSampleSize):
+        figure += calibSample[i]       
+    average = figure / calibSampleSize
+    robobit.stop(RBStopMode.BRAKE)
+    robobit.goms(RBDirection.REVERSE, 50, 1000)
     basic.show_string("D")
     global calibDone
     calibDone = True
@@ -36,50 +41,52 @@ def calibrate():
             sampleMax = value                  
         if value < sampleMin:
             sampleMin = value            
-    serial.write_value("Min: ", sampleMin)
-    serial.write_value("Max: ", sampleMax)
-    return [sampleMin, sampleMax] 
+    #serial.write_value("Min: ", sampleMin)
+    #serial.write_value("Max: ", sampleMax)
+    serial.write_value("Average", average)
+    #return [sampleMin, sampleMax]
+    return average 
 
 
 
 
-def moveAndAvoid(): 
-    robobit.led_rainbow()   
+def moveAndAvoid():       
     distance = robobit.sonar(RBPingUnit.Centimeters);    
     accelZ = input.acceleration(Dimension.Z) 
-
     #led.plot_bar_graph(input.acceleration(Dimension.Z), 1023)
-    serial.write_value("min: ", calibValues[0])
-    serial.write_value("max: ", calibValues[1])
+    #serial.write_value("min: ", calibValues[0])
+    #serial.write_value("max: ", calibValues[1])
     serial.write_value("distance: ", distance)
-    if distance <= 10 :
+    if distance <= 15 :
+        robobit.led_clear()
         serial.write_line("NEAR OBSTACLE")
         basic.show_string("O")
         robobit.stop(RBStopMode.BRAKE)
-        basic.pause(1000)
-        robobit.goms(RBDirection.REVERSE, 50, 400)
+        basic.pause(500)
+        robobit.goms(RBDirection.REVERSE, 50, 200)
         robobit.rotatems(RBRobotDirection.LEFT, 60, 400)
-    elif accelZ >= calibValues[0] and accelZ <= calibValues[1]:
+    elif accelZ >= calibAvg - 8 and accelZ <= calibAvg + 8:
+        robobit.led_clear()
         serial.write_line("STATIONARY")
         basic.show_string("S")
         robobit.stop(RBStopMode.BRAKE)
-        basic.pause(1000)
-        robobit.goms(RBDirection.REVERSE, 50, 400)
+        basic.pause(500)
+        robobit.goms(RBDirection.REVERSE, 50, 200)
         robobit.rotatems(RBRobotDirection.RIGHT, 60, 400)
     else:
         serial.write_line("MOVING")
+        robobit.led_rainbow() 
         basic.show_string("M")
         robobit.go(RBDirection.FORWARD, 50)
 
 def on_button_pressed_a():
-    calibValues = calibrate()
+    calibAvg = calibrate()
 input.on_button_pressed(Button.A, on_button_pressed_a)
 
     
 def on_forever():
-    serial.write_value("accel: ", input.acceleration(Dimension.Z))
-    #serial.write_value("Calibration done: ", calibDone)
+    serial.write_value("Accel Z: ", input.acceleration(Dimension.Z))
     if calibDone:
-        #basic.show_string("R")
+        basic.show_string("/")
         moveAndAvoid()       
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -6,23 +6,32 @@ let distance = 0
 let accelZ = 0
 let accelY = 0
 let calibValues = [0, 0]
+let calibAvg = 0
 let calibDone = false
-function calibrate(): number[] {
+let average = 0
+function calibrate(): number {
+    let i: number;
     //  This calibrates the accelermometer to detect if the robot has come to a stop               
     let sampleMax = 0
     let sampleMin = 0
     let calibSample = []
     let calibSampleSize = 100
     basic.showString("C")
-    // robobit.go(RBDirection.FORWARD, 50) 
+    robobit.go(RBDirection.Forward, 50)
     //  Push a series of samples of the accelerometer into an array, to detect a broad range of changing movements
-    for (let i = 0; i < calibSampleSize; i++) {
+    for (i = 0; i < calibSampleSize; i++) {
         calibSample.push(input.acceleration(Dimension.Z))
         // serial.write_value("values: ", calibSample[i])
         basic.pause(50)
     }
-    // obobit.stop(RBStopMode.BRAKE)
-    // robobit.goms(RBDirection.REVERSE, 50, 1000)
+    //  Get average of sample values
+    let figure = 0
+    for (i = 0; i < calibSampleSize; i++) {
+        figure += calibSample[i]
+    }
+    let average = figure / calibSampleSize
+    robobit.stop(RBStopMode.Brake)
+    robobit.goms(RBDirection.Reverse, 50, 1000)
     basic.showString("D")
     
     calibDone = true
@@ -39,35 +48,39 @@ function calibrate(): number[] {
         }
         
     }
-    serial.writeValue("Min: ", sampleMin)
-    serial.writeValue("Max: ", sampleMax)
-    return [sampleMin, sampleMax]
+    // serial.write_value("Min: ", sampleMin)
+    // serial.write_value("Max: ", sampleMax)
+    serial.writeValue("Average", average)
+    // return [sampleMin, sampleMax]
+    return average
 }
 
 function moveAndAvoid() {
-    robobit.ledRainbow()
     let distance = robobit.sonar(RBPingUnit.Centimeters)
     let accelZ = input.acceleration(Dimension.Z)
     // led.plot_bar_graph(input.acceleration(Dimension.Z), 1023)
-    serial.writeValue("min: ", calibValues[0])
-    serial.writeValue("max: ", calibValues[1])
+    // serial.write_value("min: ", calibValues[0])
+    // serial.write_value("max: ", calibValues[1])
     serial.writeValue("distance: ", distance)
-    if (distance <= 10) {
+    if (distance <= 15) {
+        robobit.ledClear()
         serial.writeLine("NEAR OBSTACLE")
         basic.showString("O")
         robobit.stop(RBStopMode.Brake)
-        basic.pause(1000)
-        robobit.goms(RBDirection.Reverse, 50, 400)
+        basic.pause(500)
+        robobit.goms(RBDirection.Reverse, 50, 200)
         robobit.rotatems(RBRobotDirection.Left, 60, 400)
-    } else if (accelZ >= calibValues[0] && accelZ <= calibValues[1]) {
+    } else if (accelZ >= calibAvg - 8 && accelZ <= calibAvg + 8) {
+        robobit.ledClear()
         serial.writeLine("STATIONARY")
         basic.showString("S")
         robobit.stop(RBStopMode.Brake)
-        basic.pause(1000)
-        robobit.goms(RBDirection.Reverse, 50, 400)
+        basic.pause(500)
+        robobit.goms(RBDirection.Reverse, 50, 200)
         robobit.rotatems(RBRobotDirection.Right, 60, 400)
     } else {
         serial.writeLine("MOVING")
+        robobit.ledRainbow()
         basic.showString("M")
         robobit.go(RBDirection.Forward, 50)
     }
@@ -75,13 +88,12 @@ function moveAndAvoid() {
 }
 
 input.onButtonPressed(Button.A, function on_button_pressed_a() {
-    let calibValues = calibrate()
+    let calibAvg = calibrate()
 })
 basic.forever(function on_forever() {
-    serial.writeValue("accel: ", input.acceleration(Dimension.Z))
-    // serial.write_value("Calibration done: ", calibDone)
+    serial.writeValue("Accel Z: ", input.acceleration(Dimension.Z))
     if (calibDone) {
-        // basic.show_string("R")
+        basic.showString("/")
         moveAndAvoid()
     }
     
