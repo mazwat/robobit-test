09f0eee5c46d3bb63856d09d4694035a79586653
Matt Watkins <matthew.watkins@network.rca.ac.uk>
2021-05-28 16:14:02 +0100

    Movement working. Some issues with 'stationary' state

--- main.py
+++ main.py
@@ -1,35 +1,50 @@
-
+# Set up model specs and accelerometor sensitivity
 robobit.select_model(RBModel.Mk3)
-#robobit.go(RBDirection.FORWARD, 60)
-#robobit.rb_enable_bluetooth(RBBluetooth.BT_ENABLE)
-#robobit.rotate(RBRobotDirection.LEFT, 60)
 input.set_accelerometer_range(AcceleratorRange.FOUR_G)
-robobit.led_rainbow()
+# Initial variables
 distance = 0
 accelZ = 0
 accelY = 0
+calibValues = [0,0]
+calibDone = False
 
-def calibrate():                  
+def calibrate():   
+    # This calibrates the accelermometer to detect if the robot has come to a stop               
     sampleMax = 0
     sampleMin = 0
     calibSample = [] 
     calibSampleSize = 100
-    basic.show_string("C") 
+    basic.show_string("C")
+    #robobit.go(RBDirection.FORWARD, 50) 
+    # Push a series of samples of the accelerometer into an array, to detect a broad range of changing movements
     for i in range(calibSampleSize):
         calibSample.append(input.acceleration(Dimension.Z)) 
-        basic.pause(100)
-        
-    #sampleMax = max(calibSample)
+        #serial.write_value("values: ", calibSample[i])
+        basic.pause(50)
+
+    
+    #obobit.stop(RBStopMode.BRAKE)
+    #robobit.goms(RBDirection.REVERSE, 50, 1000)
+    basic.show_string("D")
+    global calibDone
+    calibDone = True
+    #serial.write_value("Calibration done: ", calibDone)
+    #Calculate the min and max values for the accelerometer. How much it is moving while it is revving against a wall or an object
+    serial.write_numbers(calibSample)
     for value in calibSample:
         if value > sampleMax:
-            sampleMax = value
+            sampleMax = value                  
         if value < sampleMin:
-            sampleMin = value  
-    return [sampleMin, sampleMax]     
+            sampleMin = value            
+    serial.write_value("Min: ", sampleMin)
+    serial.write_value("Max: ", sampleMax)
+    return [sampleMin, sampleMax] 
 
-calibValues = calibrate()
-def on_forever():
-           
+
+
+
+def moveAndAvoid(): 
+    robobit.led_rainbow()   
     distance = robobit.sonar(RBPingUnit.Centimeters);    
     accelZ = input.acceleration(Dimension.Z) 
 
@@ -37,15 +52,34 @@ def on_forever():
     serial.write_value("min: ", calibValues[0])
     serial.write_value("max: ", calibValues[1])
     serial.write_value("distance: ", distance)
-    if distance <= 8 and accelZ >= calibValues[0] and accelZ <= calibValues[1]:
+    if distance <= 10 :
         serial.write_line("NEAR OBSTACLE")
         basic.show_string("O")
         robobit.stop(RBStopMode.BRAKE)
         basic.pause(1000)
         robobit.goms(RBDirection.REVERSE, 50, 400)
         robobit.rotatems(RBRobotDirection.LEFT, 60, 400)
+    elif accelZ >= calibValues[0] and accelZ <= calibValues[1]:
+        serial.write_line("STATIONARY")
+        basic.show_string("S")
+        robobit.stop(RBStopMode.BRAKE)
+        basic.pause(1000)
+        robobit.goms(RBDirection.REVERSE, 50, 400)
+        robobit.rotatems(RBRobotDirection.RIGHT, 60, 400)
     else:
         serial.write_line("MOVING")
         basic.show_string("M")
         robobit.go(RBDirection.FORWARD, 50)
+
+def on_button_pressed_a():
+    calibValues = calibrate()
+input.on_button_pressed(Button.A, on_button_pressed_a)
+
+    
+def on_forever():
+    serial.write_value("accel: ", input.acceleration(Dimension.Z))
+    #serial.write_value("Calibration done: ", calibDone)
+    if calibDone:
+        #basic.show_string("R")
+        moveAndAvoid()       
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,23 +1,34 @@
+//  Set up model specs and accelerometor sensitivity
 robobit.select_model(RBModel.Mk3)
-// robobit.go(RBDirection.FORWARD, 60)
-// robobit.rb_enable_bluetooth(RBBluetooth.BT_ENABLE)
-// robobit.rotate(RBRobotDirection.LEFT, 60)
 input.setAccelerometerRange(AcceleratorRange.FourG)
-robobit.ledRainbow()
+//  Initial variables
 let distance = 0
 let accelZ = 0
 let accelY = 0
+let calibValues = [0, 0]
+let calibDone = false
 function calibrate(): number[] {
+    //  This calibrates the accelermometer to detect if the robot has come to a stop               
     let sampleMax = 0
     let sampleMin = 0
     let calibSample = []
     let calibSampleSize = 100
     basic.showString("C")
+    // robobit.go(RBDirection.FORWARD, 50) 
+    //  Push a series of samples of the accelerometer into an array, to detect a broad range of changing movements
     for (let i = 0; i < calibSampleSize; i++) {
         calibSample.push(input.acceleration(Dimension.Z))
-        basic.pause(100)
+        // serial.write_value("values: ", calibSample[i])
+        basic.pause(50)
     }
-    // sampleMax = max(calibSample)
+    // obobit.stop(RBStopMode.BRAKE)
+    // robobit.goms(RBDirection.REVERSE, 50, 1000)
+    basic.showString("D")
+    
+    calibDone = true
+    // serial.write_value("Calibration done: ", calibDone)
+    // Calculate the min and max values for the accelerometer. How much it is moving while it is revving against a wall or an object
+    serial.writeNumbers(calibSample)
     for (let value of calibSample) {
         if (value > sampleMax) {
             sampleMax = value
@@ -28,28 +39,50 @@ function calibrate(): number[] {
         }
         
     }
+    serial.writeValue("Min: ", sampleMin)
+    serial.writeValue("Max: ", sampleMax)
     return [sampleMin, sampleMax]
 }
 
-let calibValues = calibrate()
-basic.forever(function on_forever() {
+function moveAndAvoid() {
+    robobit.ledRainbow()
     let distance = robobit.sonar(RBPingUnit.Centimeters)
     let accelZ = input.acceleration(Dimension.Z)
     // led.plot_bar_graph(input.acceleration(Dimension.Z), 1023)
     serial.writeValue("min: ", calibValues[0])
     serial.writeValue("max: ", calibValues[1])
     serial.writeValue("distance: ", distance)
-    if (distance <= 8 && accelZ >= calibValues[0] && accelZ <= calibValues[1]) {
+    if (distance <= 10) {
         serial.writeLine("NEAR OBSTACLE")
         basic.showString("O")
         robobit.stop(RBStopMode.Brake)
         basic.pause(1000)
         robobit.goms(RBDirection.Reverse, 50, 400)
         robobit.rotatems(RBRobotDirection.Left, 60, 400)
+    } else if (accelZ >= calibValues[0] && accelZ <= calibValues[1]) {
+        serial.writeLine("STATIONARY")
+        basic.showString("S")
+        robobit.stop(RBStopMode.Brake)
+        basic.pause(1000)
+        robobit.goms(RBDirection.Reverse, 50, 400)
+        robobit.rotatems(RBRobotDirection.Right, 60, 400)
     } else {
         serial.writeLine("MOVING")
         basic.showString("M")
         robobit.go(RBDirection.Forward, 50)
+    }
+    
+}
+
+input.onButtonPressed(Button.A, function on_button_pressed_a() {
+    let calibValues = calibrate()
+})
+basic.forever(function on_forever() {
+    serial.writeValue("accel: ", input.acceleration(Dimension.Z))
+    // serial.write_value("Calibration done: ", calibDone)
+    if (calibDone) {
+        // basic.show_string("R")
+        moveAndAvoid()
     }
     
 })
